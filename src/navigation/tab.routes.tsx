a417c4a4890a2490158a3a3a6122554f339804e5
Daniel-Mingoranse <danielmingoranse2016@outlook.com>
2023-10-25 07:02:59 -0300

    feat: Criaçao tela Login, Onboarding, Formulario Login

--- src/navigation/tab.routes.tsx
+++ src/navigation/tab.routes.tsx
@@ -1,15 +1,20 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import MinhasReceitasScreen from "../screens/MinhasReceitasScreen";
+import Onboarding from "../screens/Onboarding"
+import SignIn from "../screens/Sign_in_Screen";
+import LoginScreen from "../screens/LoginScreean";
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="MinhasReceitas"
-      >
+      <Tab.Navigator initialRouteName="Onboarding">
+        <Tab.Screen name="Onboarding" component={Onboarding} options={{ title: '' }}/>
+        <Tab.Screen name="SignIn" component={SignIn} options={{ title: '' }}/>
+        <Tab.Screen name="LoginScreen" component={LoginScreen} options={{ title: '' }}/>
+
         <Tab.Screen name="MinhasReceitas" component={MinhasReceitasScreen} />
       </Tab.Navigator>
     </NavigationContainer>
